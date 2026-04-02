b2a172fa842e85618badb478982aee3f36bca137
Khulex-tech <thapelomkhari791@gmail.com>
2026-04-02 09:54:39 +0200

    Add PlayAudio class to play welcome audio

---
+++ PlayAudio.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Media;
+
+namespace CyberShieldSA
+{
+    internal class PlayAudio
+    {
+        public void PlayWelcome()
+        {
+            // Get project directory
+            string projectPath = AppDomain.CurrentDomain.BaseDirectory;
+
+            // Remove bin/debug folder
+            string newPath = projectPath.Replace("bin\\Debug\\", "");
+
+            // Combine path with audio file
+            string audioPath = Path.Combine(newPath, "assets", "WelcomeAudio.wav");
+
+            try
+            {
+                SoundPlayer player = new SoundPlayer(audioPath);
+                player.PlaySync(); 
+            }
+            catch
+            {
+                Console.WriteLine("Audio could not play.");
+            }
+        }
+    }
+}
